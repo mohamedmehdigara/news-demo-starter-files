e73a6d776ba0e39f3e786be761dc5b0f85db412c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-14 13:07:22 +0100

    added files

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
+package main
 
+import (
+	"html/template"
+	"log"
+
+	"net/http"
+	"os"
+
+
+	"github.com/joho/godotenv"
+
+)
+var tpl = template.Must(template.ParseFiles("index.html"))
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+
+		log.Println("Error loading .env file")
+
+	}
+
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
